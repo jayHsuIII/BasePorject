1c2bb7594901cd809ccb88995b5a4224bcbc4e94
jayHsuIII <dick22142214@gmail.com>
2017-01-14 02:53:40 +0800

    底層建置

---
+++ Models/Interface/ISys_MemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Interface
+{
+    public interface ISys_MemberRepository : IRepository<Sys_Member>
+    {
+        Sys_Member GetByID(int PID);
+    }
+}
---
+++ Models/Repository/Sys_MemberRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Repository
+{
+    public class Sys_MemberRepository : GenericRepository<Sys_Member>, ISys_MemberRepository
+    {
+        public Sys_Member GetByID(int PID)
+        {
+            return this.Get(x => x.PID == PID);
+        }
+    }
+}
